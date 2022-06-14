c03dcf77a2a936920ca07c51f41b405afdfb22f5
marcgomis17 <marcgomis74@gmail.com>
2022-06-14 00:31:47 +0000

    Liste classes

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ClasseRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ClasseController extends AbstractController
-{
+class ClasseController extends AbstractController {
     #[Route('/classe', name: 'app_classe')]
-    public function index(): Response
-    {
-        return $this->render('classe/index.html.twig', [
-            'controller_name' => 'ClasseController',
-        ]);
+    public function index(ClasseRepository $repo, PaginatorInterface $paginator, Request $request): Response {
+        $currentPage = "Liste Classes";
+        $data = $repo->findAll();
+        $classes = $paginator->paginate($data, $request->query->getInt('page', 1), 3);
+        $classes->setCustomParameters(['align' => 'right']);
+        return $this->render('classe/list_classes.html.twig', ['current_page' => $currentPage, 'classes' => $classes]);
     }
 }
--- src/Repository/ClasseRepository.php
+++ src/Repository/ClasseRepository.php
@@ -14,15 +14,12 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Classe[]    findAll()
  * @method Classe[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ClasseRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
+class ClasseRepository extends ServiceEntityRepository {
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Classe::class);
     }
 
-    public function add(Classe $entity, bool $flush = false): void
-    {
+    public function add(Classe $entity, bool $flush = false): void {
         $this->getEntityManager()->persist($entity);
 
         if ($flush) {
@@ -30,8 +27,7 @@ class ClasseRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Classe $entity, bool $flush = false): void
-    {
+    public function remove(Classe $entity, bool $flush = false): void {
         $this->getEntityManager()->remove($entity);
 
         if ($flush) {
@@ -39,28 +35,28 @@ class ClasseRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Classe[] Returns an array of Classe objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Classe[] Returns an array of Classe objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('c.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Classe
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Classe
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
